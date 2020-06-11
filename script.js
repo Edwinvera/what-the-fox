bbdc6a7a5fed9bacdf7c74cb4577468ab553bc1c
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-06-10 23:13:32 -0400

    Start Styling

---
+++ script.js
@@ -0,0 +1,23 @@
+// const randomFox = 'https://cors-anywhere.herokuapp.com/randomfox.ca/floof/'
+// const button = document.querySelector('#generate')
+// const inputSoft = document.querySelector('#soft')
+// const inputColor = document.querySelector('#favcolor')
+// const inputFruit = document.querySelector('#favfruit')
+// const inputVaca = document.querySelector('#vacay')
+
+// async function getFox() {
+//   let response = await axios.get(randomFox)
+//   // console.log(response.data.image)
+//   let spiritFox = (response.data.image)
+//   let soft = document.getElementById('#soft').value
+//   let color = document.getElementById('#favcolor').value
+//   let fruit = document.getElementById('#favfruit').value
+//   let vacay = document.getElementById('#vacay').value
+// }
+
+// function foxSpeech() {
+//   let helloFren = ''
+//   helloFren = 'Hello fren! My name is ' + inputFruit.value + 'y ' + inputSoft.value + 'z. I hail from a far away land called San ' + inputVaca.value + '.' + ' I am your Spirit Fox.' + ' Floof Furrever.'
+// }
+
+// button.addEventListener('click', getFox, false)
